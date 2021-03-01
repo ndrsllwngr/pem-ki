5dd91da4204cf2d17d6e5070442cfa70593aa7b1
deeliciouscode <schmiderdavid@gmail.com>
2021-03-01 10:59:17 +0100

    rewards changed, demos set up

--- unity/iRocketLanding24/Assets/Scripts/RocketAgent.cs
+++ unity/iRocketLanding24/Assets/Scripts/RocketAgent.cs
@@ -7,17 +7,17 @@ using System;
 
 public class RocketAgent : Agent
 {
-    // Start is called before the first frame update
     Rigidbody rBody;
+    
+    // Start is called before the first frame update
     void Start () {
         rBody = GetComponent<Rigidbody>();
     }
-
-    public float forceMultiplier = 10;
-    public float thrustMultipler = 30;
+    
+    public float thrustMultipler = 20;
     public float rotationMultiplier = 3;
     public Transform Target;
-    public float oldDistance = 999;
+    public float oldDistanceX = 999;
     public override void OnActionReceived(float[] actionBuffers)
     {
         //Debug.Log(actionBuffers);
@@ -38,32 +38,76 @@ public class RocketAgent : Agent
             rBody.AddTorque(Vector3.forward * -rotationMultiplier);
         }
         
-       //// Rewards
-       float distanceToTarget = Math.Abs(this.transform.localPosition.x - Target.localPosition.x);
+       // Rewards
+       float distanceToTargetX = Math.Abs(this.transform.localPosition.x - Target.localPosition.x);
 
-       if (oldDistance == 999)
+       if (oldDistanceX == 999)
        {
-           oldDistance = distanceToTarget;
+           oldDistanceX = distanceToTargetX;
        }
 
-       float distanceDelta = oldDistance - distanceToTarget;
+       
+       float distanceDeltaX = oldDistanceX - distanceToTargetX;
+
+       if (Math.Abs(distanceDeltaX) <= 0.001)
+       {
+           //Debug.Log("didnt move, " + distanceDeltaX);
+           SetReward(-0.001f);
+       }
+       
+       if (oldDistanceX > distanceToTargetX)
+       {
+            SetReward(0.1f);
+       }
+       else
+       {
+           SetReward(-0.001f);
+       }
 
        float angle = this.transform.eulerAngles.z;
-
-       if (angle < 330 && angle > 30)
+       if (angle < 320 && angle > 40)
        {
            float diff = 180 - Math.Abs(180 - angle);
-           SetReward(-diff/90);
+           SetReward(-diff/900);
+       }
+       else if (angle < 40)
+       {
+           SetReward((40 - angle) / 1000);
+       }
+       else if (angle > 320)
+       {
+           SetReward(Math.Abs(320 - angle) / 1000);
        }
        
-       // Debug.Log("angles: " + this.transform.eulerAngles + " dist: " + distanceDelta);
-       
-       SetReward(distanceDelta);
+       if (angle < 280 && angle > 80)
+       {
+           //Debug.Log(angle);
+           SetReward(-1);
+           EndEpisode();
+       }
 
-       oldDistance = distanceToTarget;
+       float angularVelocity = rBody.angularVelocity.z;
+
+       //Debug.Log("angular velocity: " + angularVelocity);
+
+       if (Math.Abs(angularVelocity) > 0.3)
+       {
+           SetReward(-Math.Abs(angularVelocity / 5));
+       }
+
+       // Debug.Log("angles: " + this.transform.eulerAngles + " dist: " + distanceDelta);
+
+       oldDistanceX = distanceToTargetX;
+       
+       // to far away
+       if (distanceToTargetX > 200)
+       {
+           SetReward(-1.0f);
+           EndEpisode();
+       }
        
        // Fell off platform
-       if (this.transform.localPosition.y < -5)
+       if (this.transform.localPosition.y < -50.0f)
        {
            SetReward(-1.0f);
            EndEpisode();
@@ -73,7 +117,7 @@ public class RocketAgent : Agent
     {
         if (collision.gameObject.CompareTag("TargetPad"))
         {
-            SetReward(4);
+            SetReward(10);
             EndEpisode();
         }
     }
@@ -82,26 +126,20 @@ public class RocketAgent : Agent
         // Target and Agent positions
         sensor.AddObservation(Target.localPosition);
         sensor.AddObservation(this.transform.localPosition);
+        sensor.AddObservation(this.transform.eulerAngles.z);
 
         // Agent velocity
+        sensor.AddObservation(rBody.angularVelocity.z);
         sensor.AddObservation(rBody.velocity.x);
         sensor.AddObservation(rBody.velocity.y);
     }
     
-    
     public override void OnEpisodeBegin()
     {
-       // If the Agent fell, zero its momentum
-        if (this.transform.localPosition.y < -5)
-        {
-            this.rBody.angularVelocity = Vector3.zero;
-            this.rBody.velocity = Vector3.zero;
-            this.transform.localPosition = new Vector3( 7, 10, 5);
-            this.transform.rotation = Quaternion.identity;
-        }
-
-        // Move the target to a new spot
-        //Target.localPosition = new Vector3(Random.value * 8 - 4,
+        this.rBody.angularVelocity = Vector3.zero;
+        this.rBody.velocity = Vector3.zero;
+        this.transform.localPosition = new Vector3( 7, 10, 5);
+        this.transform.rotation = Quaternion.identity;
     }
     
     public override void Heuristic(float[] actionsOut)
